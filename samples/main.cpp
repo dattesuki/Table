7eda1e2b37b2a9ce7db5e70748e8e6f08024b286
dattesuki <dattesuki@mail.ru>
2025-04-04 00:33:00 +0300

    Update main.cpp

--- samples/main.cpp
+++ samples/main.cpp
@@ -14,12 +14,12 @@ size_t BradysHash(size_t key, double val) {
 using namespace std;
 int main()
 {
-	HashTable<double> BradysTableSin(90, &BradysHash);
-	for (size_t i = 0; i < 91; ++i) {
+	HashTable<double> BradysTableSin(361, &BradysHash);
+	for (size_t i = 0; i < 361; ++i) {
 		BradysTableSin.insert(i, sin(double(i) * M_PI / 180));
 	}
 	cout << "Bradys Table for sin: \n";
-	for (size_t i = 0; i < 91; ++i) {
+	for (size_t i = 0; i < 361; ++i) {
 		cout << (BradysTableSin.find(i))->first<<"\370 = "<< (BradysTableSin.find(i))->second << endl;
 	}
 
